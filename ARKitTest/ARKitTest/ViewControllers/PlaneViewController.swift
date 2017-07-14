51d6b91c4f8c529c00ad925ed1b983693cbe848e
Mark Struzinski <mark.struzinski@lowes.com>
2017-07-13 20:44:44 -0400

    Added plane detection and grid when plane is shown

---
+++ ARKitTest/ARKitTest/ViewControllers/PlaneViewController.swift
@@ -0,0 +1,73 @@
+//
+//  PlaneViewController.swift
+//  ARKitTest
+//
+//  Created by Struzinski, Mark - Mark on 7/13/17.
+//  Copyright © 2017 mstruzinski. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class PlaneViewController: UIViewController {
+    @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        sceneView.debugOptions = [
+            ARSCNDebugOptions.showFeaturePoints,
+            ARSCNDebugOptions.showWorldOrigin
+        ]
+        
+        sceneView.delegate = self
+        
+        let scene = SCNScene()
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingSessionConfiguration()
+        configuration.planeDetection = .horizontal
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+}
+
+extension PlaneViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {
+            return
+        }
+        
+        DispatchQueue.main.async {
+            self.label.text = "Plane Detected"
+            
+            UIView.animate(withDuration: 3.0, animations: {
+                self.label.alpha = 1.0
+            }, completion: { _ in
+                self.label.alpha = 0.0
+            })
+        }
+        
+        let plane = OverlayPlane(anchor: planeAnchor)
+        node.addChildNode(plane)
+    }
+}
---
+++ ARKitTest/ARKitTest/Views/OverlayPlane.swift
@@ -0,0 +1,55 @@
+//
+//  OverlayPlane.swift
+//  ARKitTest
+//
+//  Created by Struzinski, Mark - Mark on 7/13/17.
+//  Copyright © 2017 mstruzinski. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class OverlayPlane: SCNNode {
+    var anchor: ARPlaneAnchor
+    var planeGeometry: SCNPlane!
+    
+    init(anchor: ARPlaneAnchor) {
+        self.anchor = anchor
+        super.init()
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func update(withAnchor anchor: ARPlaneAnchor) {
+        self.planeGeometry.width = CGFloat(anchor.extent.x)
+        self.planeGeometry.height = CGFloat(anchor.extent.z)
+        self.position = SCNVector3Make(anchor.center.x,
+                                       0,
+                                       anchor.center.z)
+    }
+    
+    private func setup() {
+        planeGeometry = SCNPlane(width: CGFloat(self.anchor.extent.x),
+                                 height: CGFloat(self.anchor.extent.z))
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "overlay_grid")
+        
+        planeGeometry.materials = [
+            material
+        ]
+        
+        let planeNode = SCNNode(geometry: planeGeometry)
+        planeNode.position = SCNVector3Make(anchor.center.x,
+                                            0,
+                                            anchor.center.z)
+        planeNode.transform = SCNMatrix4MakeRotation(Float(-Double.pi / 2.0),
+                                                     1.0,
+                                                     0.0,
+                                                     0.0)
+        self.addChildNode(planeNode)
+    }
+}
